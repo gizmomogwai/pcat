400954dbb41940b99b9946717a0e5abc4ef2aca6
Christian Koestlin <christian.koestlin@esrlabs.com>
2019-03-30 21:32:50 +0100

    Use cycle to choose from colors

--- source/app.d
+++ source/app.d
@@ -3,6 +3,7 @@ import std.algorithm;
 import std.concurrency;
 import std.datetime;
 import std.stdio;
+import std.range;
 
 void read(string fileName, AnsiColor color)
 {
@@ -16,26 +17,31 @@ void read(string fileName, AnsiColor color)
     // dfmt on
 }
 
+auto advance(R)(ref R r)
+{
+    auto res = r.front;
+    r.popFront;
+    return res;
+}
+
 void main(string[] args)
 {
-    enum colors = [
-            AnsiColor.red, AnsiColor.green, AnsiColor.blue, AnsiColor.yellow,
-            AnsiColor.cyan, AnsiColor.magenta
-        ];
-    args[1 .. $].each!((i, fileName) => spawnLinked(&read, fileName, colors[i % colors.length]));
+    auto colors = cycle([AnsiColor.red, AnsiColor.green, AnsiColor.blue,
+            AnsiColor.yellow, AnsiColor.cyan, AnsiColor.magenta]);
+    args[1 .. $].each!(fileName => spawnLinked(&read, fileName, advance(colors)));
     auto h = args.length - 1;
     while (h > 0)
     {
         // dfmt off
         receive(
-            (AnsiColor color, string channel, string timestamp, string message)
-            {
-                writeln(new StyledString("%-26s %s: %s".format(timestamp, channel, message)).setForeground(color));
-            },
-            (LinkTerminated terminated)
-            {
-                h--;
-            },
+          (AnsiColor color, string channel, string timestamp, string message)
+          {
+              writeln(new StyledString("%-26s %s: %s".format(timestamp, channel, message)).setForeground(color));
+          },
+          (LinkTerminated terminated)
+          {
+              h--;
+          },
         );
         // dfmt on
     }
